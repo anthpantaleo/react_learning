9a635432fe34be32137e30b6ba852759cb7aab9e
anthpantaleo <anthpantaleo@gmail.com>
2022-11-29 18:43:54 -0500

    having trouble following, need written resources

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -13,15 +13,17 @@ const ExpenseItem = (props) => {
   // };
 
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
-      <button>Change Title</button>
-      {/* removed  onClick={clickHandler} from button and removed state */}
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+        <button>Change Title</button>
+        {/* removed  onClick={clickHandler} from button and removed state */}
+      </Card>
+    </li>
   );
 };
 
