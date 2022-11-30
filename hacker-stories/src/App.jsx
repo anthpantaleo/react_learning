a41d0b3a1af5ccd8dd0b5333d7e35dd9e14c3805
anthpantaleo <anthpantaleo@gmail.com>
2022-11-30 14:01:52 -0500

    lists in react

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -3,12 +3,37 @@
 // import "./App.css";
 import * as React from "react";
 
-const title = "React!";
-
 const welcome = {
   greeting: "Hey",
   title: "React",
 };
+
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+  {
+    title: "Google",
+    url: "https://google.ca",
+    author: "AI",
+    num_comments: 1000000,
+    points: 0,
+    objectID: 2,
+  },
+];
 
 function App() {
   return (
@@ -16,8 +41,27 @@ function App() {
       <h1>
         {welcome.greeting}, {welcome.title}
       </h1>
+
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+
+      <hr />
+      <ul>
+        {list.map((item) => {
+          return (
+            <li key={item.objectID}>
+              <span>
+                <a href={item.url} target="_blank">
+                  {item.title}
+                </a>
+              </span>
+              <span> - {item.author}</span>
+              <span> - {item.num_comments}</span>
+              <span> - {item.points}</span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
